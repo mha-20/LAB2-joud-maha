b96c658fa315e0550f17d56317ecfed06b4d4244
ccm-stu <s202366370@kfupm.edu.sa>
2025-01-26 16:31:12 +0300

    okay I will add it

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Joud");
         System.out.println("New Branch");
+        System.out.println("Hello Maha");
     }
     }
 
